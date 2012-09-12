76174595fecf170c6921319d5ce979aa21394710
dawidburek@gmail.com <dawidburek@gmail.com@7d77e253-9e1a-ffef-550d-f032805ebbe6>
2012-09-12 16:48:31 +0000

    First commit. Project is already ready to be used in commercial projects.

---
+++ trunk/ErrorMsg/ErrorMsg/ErrorMsg.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DawidBurek.ErrorMsg
+{
+    public enum ErrorMsgIcon { Information, Question, Critical, Exclamation, Stop }
+
+    public static class ErrorMsg
+    {
+        #region Fields
+        //if 3 variables below are not set, default values 
+        //are assigned to then in FormErrorMessage constructor.
+        public static string ErrorDetails = "";
+        public static string EmailRecipient = "";
+        public static string EmailSubject = "";
+        #endregion
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <returns></returns>
+        public static DialogResult Show()
+        {
+            FormErrorMessage _form = new FormErrorMessage();
+
+            return _form.ShowDialog();
+        }
+
+        /// <summary>
+        /// Constructor with message text as parameter.
+        /// </summary>
+        /// <param name="message">Message text to show and copy to clipboard/send as an e-mail</param>
+        /// <returns></returns>
+        public static DialogResult Show(string message)
+        {
+            FormErrorMessage _form = new FormErrorMessage();
+
+            _form.LabelMessage.Text = message;
+            return _form.ShowDialog();
+        }
+
+        /// <summary>
+        /// Constructor with window title and message text as parameters.
+        /// </summary>
+        /// <param name="message">Message text to show and copy to clipboard/send as an e-mail</param>
+        /// <param name="title">Window title</param>
+        /// <returns></returns>
+        public static DialogResult Show(string message, string title)
+        {
+            FormErrorMessage _form = new FormErrorMessage();
+
+            _form.LabelMessage.Text = message;
+            _form.Text = title;
+
+            return _form.ShowDialog();
+        }
+
+        /// <summary>
+        /// Constructor with window title, message text and icon type as parameters.
+        /// </summary>
+        /// <param name="message">Message text to show and copy to clipboard/send as an e-mail</param>
+        /// <param name="title">Window title</param>
+        /// <param name="iconType">Icon type</param>
+        /// <returns></returns>
+        public static DialogResult Show(string message, string title, ErrorMsgIcon iconType)
+        {
+            FormErrorMessage _form = new FormErrorMessage();
+
+            _form.LabelMessage.Text = message;
+            _form.Text = title;
+            _form.SetIcon(iconType);
+
+            return _form.ShowDialog();
+        }
+
+        /// <summary>
+        /// Constructor with window title, message text, icon type and error details as parameters.
+        /// </summary>
+        /// <param name="message">Message text - shown on dialogbox and copied to clipboard/sent as an e-mail</param>
+        /// <param name="title">Window title</param>
+        /// <param name="iconType">Icon type</param>
+        /// <param name="errorDetails">Error details - copied to clipboard/sent as an e-mail</param>
+        /// <returns></returns>
+        public static DialogResult Show(string message, string title, ErrorMsgIcon iconType, string errorDetails)
+        {
+            FormErrorMessage _form = new FormErrorMessage();
+
+            _form.LabelMessage.Text = message;
+            _form.Text = title;
+            _form.SetIcon(iconType);
+            ErrorDetails = errorDetails;
+            _form.LLReport.Visible = true;
+
+            return _form.ShowDialog();
+        }    
+    }
+}
---
+++ trunk/ErrorMsg/Test/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DawidBurek.ErrorMsg;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ErrorMsg.EmailRecipient = "a@a.a";
+            ErrorMsg.EmailSubject = "Temat maila";
+            ErrorMsg.ErrorDetails = "szczegoly \n\n\nbledu...";
+
+            MessageBox.Show("info", "text");//, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //ErrorMsg.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ErrorMsg.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ErrorMsg.Show("tresc");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ErrorMsg.Show("tresc", "tytul");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ErrorMsg.Show("tresc", "tytul", ErrorMsgIcon.Information);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ErrorMsg.Show(richTextBox1.Text, "tytul", ErrorMsgIcon.Information, richTextBox1.Text);
+        }
+    }
+}
